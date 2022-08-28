e26771127ea1c33fd31879321f051583655f69f0
Hapu2001 <91301641+Hapu2001@users.noreply.github.com>
2022-08-28 10:15:51 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <React.StrictMode>
-    <Router>
+    <HashRouter >
       <section className="font-poppins">
       <App />
       </section>
-    </Router>
+    </HashRouter >
   </React.StrictMode>
   
 );
