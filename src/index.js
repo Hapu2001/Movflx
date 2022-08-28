b82e5dc20ff58c840db203243becffbce50c1104
Hapu2001 <91301641+Hapu2001@users.noreply.github.com>
2022-08-28 10:33:28 +0700

    fix HashRouter

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <section className="font-poppins">
       <App />
       </section>
-    </Router>
+    </HashRouter>
   </React.StrictMode>
   
 );
